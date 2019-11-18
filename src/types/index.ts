ad16362271f6b4249f9e215af8ffdfdccef75956
吴呈呈 <woochon@163.com>
2019-11-18 23:29:29 +0800

    xhr

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type Method = 'get' | 'GET'
+| 'delete' | 'DELETE'
+| 'post' | 'POST'
+| 'head' | 'HEAD'
+| 'options' | 'OPTIONS'
+| 'put' | 'PUT'
+| 'patch' | 'PATCH'
+export interface  AxiosRequestConfig {
+  url:string
+  method?:Method
+  data?:any
+  params?:any
+}
