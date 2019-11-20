28a17212b4a4a987623e9c586e8f7f187381aba1
吴呈呈 <woochon@163.com>
2019-11-20 11:23:57 +0800

    axios请求设置

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import {AxiosRequestConfig} from "./types";
 import { buildURL} from "./helpers/url";
 import {transformRequest} from "./helpers/data";
+import {processHeaders} from "./helpers/headers";
 import xhr from './xhr';
 
 function axios(config:AxiosRequestConfig){
   processConfig(config);
+  config.headers = transformHeaders(config);
   xhr(config);
 }
 
@@ -20,6 +22,11 @@ function transformURL(config:AxiosRequestConfig):string{
 
 function transRequestData(config:AxiosRequestConfig):any{
   return transformRequest(config.data)
+}
+
+function transformHeaders(config:AxiosRequestConfig):any{
+  const {headers = {} ,data} = config;
+  return processHeaders(headers,data);
 }
 
 export default axios;
--- src/types/index.ts
+++ src/types/index.ts
@@ -10,4 +10,14 @@ export interface  AxiosRequestConfig {
   method?:Method
   data?:any
   params?:any
+  headers?:any
+}
+
+export interface AxiosReponse {
+  data:any
+  status:number
+  statusText:string
+  headers:any
+  config:AxiosRequestConfig
+  request:any
 }
